669e3460c183cc056dfc3a54a2f16832c02c27d9
Siddharth Saran <siddharth.saran@gmail.com>
2024-01-24 00:33:34 -0500

    update-env updated for new parameter value

--- update-env.sh
+++ update-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "DATABASE_URL=$(aws ssm get-parameter --name 'DATABASE_URL' --with-decryption | jq '.Parameter.Value')" >> .env
+echo "DATABASE_URL=$(aws ssm get-parameter --name '/amplify/d17peozzagnvp9/beta/AMPLIFY_alpha_beta_DATABASE_URL' --with-decryption | jq '.Parameter.Value')" >> .env
