7c8be2c7d49be6e9e6043759e643d782a3413ac8
Siddharth Saran <siddharth.saran@gmail.com>
2024-01-25 14:05:49 -0500

    New amplify app

--- update-env.sh
+++ update-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "DATABASE_URL=$(aws ssm get-parameter --name '/amplify/d17peozzagnvp9/main/AMPLIFY_alpha_alpha_DATABASE_URL' --with-decryption | jq '.Parameter.Value')" >> .env
+echo "DATABASE_URL=$(aws ssm get-parameter --name '/amplify/d3rrpjmy68f0j1/main/AMPLIFY_alpha_alpha_DATABASE_URL' --with-decryption | jq '.Parameter.Value')" >> .env
